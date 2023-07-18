4bc6da8e441b516eb96a2bd6e3ea0b1e01ae93b5
smurf <smurf@kakao.com>
2023-07-19 00:04:13 +0900

    pick

--- TS/0718 Utility Types/0718-Pick.ts
+++ TS/0718 Utility Types/0718-Pick.ts
@@ -19,7 +19,10 @@ const OmitTest: Omit<Profile, "married"> = {
 
 // Pick Omit 직접 만들기
 
-type CustomPick<T, S> = {
-  [Key in keyof S]: S[Key];
+type CustomPick<T, S extends keyof T> = {
+  // [Key in keyof S]: S[Key]; => S[Key] 는 S가 이미 지정된 객체키기때문에
+  // prop1:'prop1', prop2:'prop2' 처럼됨
+  [Key in S]: T[Key];
 };
+// 제네릭을 설정할때 , 서로와 관계를 먼저 정해줘야한다. (제한조건을 먼저
 type CustomOmit<T, S> = {};
